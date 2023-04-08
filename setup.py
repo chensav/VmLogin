98a3974e0bcb6c59090fba927262c18f6b71af04
chensav <mr.chenwangfa@aliyun.com>
2023-04-08 18:40:52 +0800

    增加测试

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/chensav/VmLogin'
 EMAIL = 'chensav@163.com'
 AUTHOR = 'chensav'
 REQUIRES_PYTHON = '>=3.8.16'
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
